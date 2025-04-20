b6d4c07ed65f17f56bce234932b323d67aec5b9c
Allan <allanbarongo5@gmail.com>
2025-04-20 23:54:50 +0300

    Completed js-TDD writing-unit-tests lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,49 @@
-// Your tests here
+
+import { pointsForWord,isPalindrome } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+    const word = "test";
+
+    const points = pointsForWord(word);
+
+    expect(points).toBe(7);
+  });
+
+  it("handles uppercase and lowercase input", () => {
+    const word = "tEsT";
+  
+    const points = pointsForWord(word);
+  
+    expect(points).toBe(7);
+  });
+});
+
+describe("isPalindrome",()=>{
+    it("Checks if a word is a palidrome and returns 'true' if it is",()=>{
+        const word="racecar";
+
+        const check=isPalindrome(word);
+        
+        expect(check).toBe(true)
+    })
+
+    it("should return true for words that are a combination of uppercase and lowercase letters",()=>{
+        const word="rAceCar";
+
+        const check=isPalindrome(word);
+        
+        expect(check).toBe(true)
+    })
+    
+    it("throws an error if the input has any non-alphabetic characters", () => {
+        const word = "race car";
+    
+        expect(() => isPalindrome(word)).toThrow();
+      });
+      it("throws an error if the input is an empty string", () => {
+        const word = "";
+    
+        expect(() => isPalindrome(word)).toThrow();
+      });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-// Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/i.test(char)? 1 : 2;
+    }
+    return points;
+  }
+
+export function isPalindrome (word){
+    const reversed=word.split('').reverse().join('').toLowerCase()
+    if(!/^[A-Za-z]+$/.test(word)){
+        throw new Error("Enter valid input");
+    }
+    else{
+    if(word.toLowerCase()===reversed){
+            return true}
+}}
